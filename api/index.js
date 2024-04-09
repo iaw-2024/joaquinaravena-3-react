014d5512432090b0b63dc3c7c3a1f1d52ba8a08e
Joaquin <joaco.aravena02@gmail.com>
2024-04-09 12:34:54 -0300

    Cambio en datos

--- api/index.js
+++ api/index.js
@@ -1,21 +1,8 @@
-const jugadores = [
-  { id: 12, nombre: "Leandro Brey", posicion: "Arquero" },
-  { id: 42, nombre: "Lucas Blondel", posicion: "Defensor" },
-  { id: 38, nombre: "Aaron Anselmino", posicion: "Defensor" },
-  { id: 6, nombre: "Marcos Rojo", posicion: "Defensor" },
-  { id: 23, nombre: "Lautaro Blanco", posicion: "Defensor" },
-  { id: 21, nombre: "Equi Fernandez", posicion: "Mediocampista" },
-  { id: 47, nombre: "Jabes Saralegui", posicion: "Mediocampista" },
-  { id: 22, nombre: "Kevin Zenón", posicion: "Mediocampista" },
-  { id: 39, nombre: "Vicente Taborda", posicion: "Mediocampista" },
-  { id: 16, nombre: "Miguel Merentiel", posicion: "Delantero" },
-  { id: 10, nombre: "Edinson Cavani", posicion: "Delantero" }
-];
-
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3000;
+const jugadores = require('./datos.json');
 
 
 app.use(express.static(path.join(__dirname, '../cliente/build')));
@@ -24,11 +11,7 @@ app.listen(PORT, () => console.log(`Servidor Express corriendo en el puerto ${PO
 
 //datos
 app.get('/api/jugadores' , (req, res) => {
-    console.log("Llamado!!")
-    const data = {
-      jugadores: jugadores
-    };
-    res.json(data);
+    res.send(jugadores);
   });
 
 module.exports = app;
